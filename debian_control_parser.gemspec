be20cd601b2eedb52cad3e3299ba659b450c07f8
Christoph Haas <email@christoph-haas.de>
2014-09-12 12:38:41 +0200

    First commit - work in progress

---
+++ debian_control_parser.gemspec
@@ -0,0 +1,18 @@
+Gem::Specification.new do |s|
+  s.name        = 'debian_control_parser'
+  s.version     = '0.1'
+  s.date        = '2014-09-11'
+  s.summary     = "Parses Debian control files"
+  s.description = <<-EOF
+    Iterates over Debian control files. As control files can be very
+    large this gem provides iterators for each block and each pair of
+    key/values in the file.
+    EOF
+  s.author     = 'Christoph Haas'
+  s.email      = 'email@christoph-haas.de'
+  s.files      = ["lib/debian_control_parser.rb"]
+  s.homepage   =
+    'http://github.com/Signum/debian-control-parser-gem'
+  s.license       = 'MIT'
+end
+
---
+++ lib/debian_control_parser.rb
@@ -0,0 +1,48 @@
+module DebianControlParser
+  # Iterator that splits up the input of a debian control file
+  # by empty lines. For example Debian "Packages" files consist
+  # of one block for each package listed in it.
+  def blocks(data)
+    gathered_lines = ''
+    data.each_line do |line|
+      if line.chomp.empty?  # empty line found that seperates blocks
+        unless gathered_lines.empty?  # any lines gathered so far?
+          yield gathered_lines  # return the block
+          gathered_lines = ''
+        end
+      else
+        gathered_lines << line
+      end
+    end
+
+    # Any lines left after the last empty line and the end of the input?
+    unless gathered_lines.empty?
+      yield gathered_lines
+    end
+  end
+
+  # Iterator that reads a block of lines from a control file. If the file
+  # consists of several empty-line-seperated blocks it must first be
+  # split by using the 'blocks' iterator.
+  def parse(data)
+    key=value=''
+    data.each_line do |line|
+      case line
+      when /^(\w+): (.+)/ # "Key: Value"
+        yield(key,value) unless value.empty?
+        key,value=$1,$2
+      when /^(\w+):\s*/   # "Key:" (start of multi-line entry)
+        yield(key,value) unless value.empty?
+        key=$1
+        value=''
+      when /^\s+(.+)/     # " Indented multi-line value"
+        value << $1+"\n"
+      else
+        puts "Unexpected line found: #{line}"
+      end
+    end
+
+    # Any lines left at the end of the input?
+    yield(key,value) unless value.empty?
+  end
+end
