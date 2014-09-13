d1b0d1647a3d39c57453d712e642bca1f19333bf
Christoph Haas <email@christoph-haas.de>
2014-09-13 15:34:51 +0200

    Renamed methods
    blocks -> paragraphs
    parse -> fields
    
    This terminology matches the Debian policy.

--- lib/debian_control_parser.rb
+++ lib/debian_control_parser.rb
@@ -2,10 +2,12 @@ module DebianControlParser
   # Iterator that splits up the input of a debian control file
   # by empty lines. For example Debian "Packages" files consist
   # of one block for each package listed in it.
-  def self.blocks(data)
+  def self.paragraphs(data)
+    return enum_for(:paragraphs) unless block_given?
+
     gathered_lines = ''
     data.each_line do |line|
-      if line.chomp.empty?  # empty line found that seperates blocks
+      if line.chomp.empty?  # empty line found that seperates paragraphs
         unless gathered_lines.empty?  # any lines gathered so far?
           yield gathered_lines  # return the block
           gathered_lines = ''
@@ -21,19 +23,21 @@ module DebianControlParser
     end
   end
 
-  # Iterator that reads a block of lines from a control file. If the file
-  # consists of several empty-line-seperated blocks it must first be
-  # split by using the 'blocks' iterator.
-  def self.parse(data)
-    key=value=''
+  # Iterator that reads a paragraph from a control file. If the file
+  # consists of several paragraphs (seperated by empty lines)
+  # it must first be split by using the 'paragraphs' iterator.
+  def self.fields(data)
+    return enum_for(:fields) unless block_given?
+
+    name=value=''
     data.each_line do |line|
       case line
       when /^(.+): (.+)/ # "Key: Value"
-        yield(key,value) unless value.empty?
-        key,value=$1,$2
+        yield(name,value) unless value.empty?
+        name,value=$1,$2
       when /^(.+):\s*/   # "Key:" (start of multi-line entry)
-        yield(key,value) unless value.empty?
-        key=$1
+        yield(name,value) unless value.empty?
+        name=$1
         value=''
       when /^\s+(.+)/     # " Indented multi-line value"
         value << $1+"\n"
@@ -41,6 +45,6 @@ module DebianControlParser
     end
 
     # Any lines left at the end of the input?
-    yield(key,value) unless value.empty?
+    yield(name,value) unless value.empty?
   end
 end
